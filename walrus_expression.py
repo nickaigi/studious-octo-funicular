0b24cbe568db102a40cc1624c08dd17648ee6ee0
Nickson Kaigi <nickaigi@gmail.com>
2020-08-25 18:35:35 +0300

    old while loop style

--- walrus_expression.py
+++ walrus_expression.py
@@ -3,6 +3,9 @@ class Walrus(object):
     Assingment Expressions aka Walrus expression :=
     - Assignment expressions allow you to assign and return a value in the same
       expression
+
+    - most convenient when using 'while' loops where you need to initialize and
+      update a variable
     """
 
     def old_example_one(self):
@@ -13,3 +16,10 @@ class Walrus(object):
     def example_one(self):
         """ with assignment expression """
         print(walrus := True)
+
+    def old_example_two_with_repetition(self):
+        inputs = list()
+        current = input("Write something: ")
+        while current != "quit":
+            inputs.append(current)
+            current = input("Write something: ")
