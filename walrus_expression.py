95db06f677d398a9fe30f3ce3326a5eb7ed92f76
Nickson Kaigi <nickaigi@gmail.com>
2020-08-25 17:38:00 +0300

    initial import walrus expression

---
+++ walrus_expression.py
@@ -0,0 +1,9 @@
+class Walrus(object):
+    """
+    Assingment Expressions aka Walrus expression :=
+    - Assignment expressions allow you to assign and return a value in the same
+      expression
+    """
+
+    def example_one(self):
+        print(walrus := True)
