0ec0e2d27cb8b57a9d92e7b7a2edcafa1c640ae9
Nickson Kaigi <nickaigi@gmail.com>
2020-08-28 17:53:15 +0300

    self documenting f-strings

--- python38/f-strings.py
+++ python38/f-strings.py
@@ -6,6 +6,7 @@
 """
 
 import math
+from datetime import date
 
 class Comedian(object):
     def __init__(self, first_name, last_name, age):
@@ -14,10 +15,36 @@ class Comedian(object):
         self.age = age
 
     def __str__(self):
+        """
+        string returned by __str__() is the informal representation of an object
+        and should be readable
+        call str() to trigger a call to __str__()
+        """
         return f'{self.first_name} {self.last_name} is {self.age}.'
 
     def __repr__(self):
+        """
+        string returned by __repr__() is the official representation and should be
+        unambiguous
+        call repr() to trigger a call to __repr__()
+        """
         return f'{self.first_name} {self.last_name} is {self.age}. Suprise!'
+
+
+def self_documenting_expressions():
+    """
+    f-strings have an '=' specifier such as f'{expr=}' which expands to the
+    text of the expression, an equal sign, then representation of the evaluated
+    expression
+    """
+    user = 'nickson'
+    member_since = date(1975, 2, 28)
+    print(f'{user=} {member_since=}')
+
+    # f-string format specifiers allow more control over how the result of the
+    # expression is displayed
+    delta = date.today() - member_since
+    print(f'{user=!s} {delta.days=:,d}')
 
 
 def main():
@@ -34,6 +61,12 @@ def main():
     # you could use objects created from classes with f-strings
     new_comedian = Comedian('Nick', 'Son', 32)
     print(f'{new_comedian}')
+
+    # by default f-strings will use __str__()
+    # f-strings can make use of __repr__() if you include the conversion flag
+    # !r
+    print(f'{new_comedian!r}')
+    self_documenting_expressions()
 
 
 if __name__ == '__main__':
