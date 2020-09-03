9f35c9416ce1fafcfac32f560048abc1658e8cfa
Nickson Kaigi <nickaigi@gmail.com>
2020-09-03 20:30:30 +0300

    added new method

--- python38/draw_line.py
+++ python38/draw_line.py
@@ -1,4 +1,5 @@
 # continuation of precise type topic
+from typing import Literal
 
 def draw_line(direction: str) -> None:
     if direction == 'horizontal':
@@ -9,6 +10,18 @@ def draw_line(direction: str) -> None:
         print('drawing a vertical line')
     else:
         raise ValueError(f'invalid direction {direction!r}')
+
+
+def draw_line_new(direction: Literal['horizontal', 'vertical']) -> None:
+    if direction == 'horizontal':
+        # draw a horizontal line
+        print('drawing horizontal line')
+    elif direction == 'vertical':
+        # draw a vertical line
+        print('drawing a vertical line')
+    else:
+        raise ValueError(f'invalid direction {direction!r}')
+
 
 if __name__ == '__main__':
     draw_line('up')
