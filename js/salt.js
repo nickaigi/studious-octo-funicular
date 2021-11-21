70ab8101ad4588efc2cb50ff9b6e48933ba1164e
Nickson Kaigi <nickaigi@gmail.com>
2021-11-21 19:32:25 +0300

    finished the salt method

--- js/salt.js
+++ js/salt.js
@@ -7,7 +7,7 @@
 // A salt is just a random value that is added to the password before it is
 // hashed
 
-const { scryptSync, randomBytes } = require('crypto');
+const { scryptSync, randomBytes, timingSafeEqual } = require('crypto');
 
 function signup(email, password){
     const salt = randomBytes(16).toString('hex');
@@ -16,8 +16,22 @@ function signup(email, password){
     // recommended to be 64
     const hashedPassword = scryptSync(password, salt, 64);
     // we store the hash password along with the salt in the database
+    // by just prepennding it to the existing password and separated by
+    // a semi-colon
     const user = { email, password: `${salt}:${hashedPassword}` }
 }
 
 function login(email, password){
+    const user = users.find(v => v.email === email);
+    const [salt, key] = user.password.split(':');
+    // we could just compare the strings here... but we are going to use
+    // the timingSafeEqual function which can prevent timing attacks
+    const hashedBuffer = scryptSync(password, salt, 64);
+    //
+    // timing attack: a hacker measures the amount of time it takes to
+    // perform an operation to obtain information about the value
+    //
+    // timingSafeEqual helps prevent from such attacks
+    const keyBuffer = Buffer.from(key, 'hex');
+    const match = timingSafeEqual(hashedBuffer, keyBuffer);
 }
